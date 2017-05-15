c7f36748b12bd470a26fcb112b8d030876b3699c
healerjean <mxzdhealer@gmail.com>
2017-05-15 16:27:30 +0800

    delete

--- 资料/2.拷贝项目/3 mnx-api-recruiter/mnx-api-recruiter/mnx-api-recruiter-master-21e41e55d3675ed5d328b3904219a445b710cfe5/src/main/java/com/minixiao/api/recruiter/controller/candidate/HandleHistoryCommand.java
+++
@@ -1,9 +0,0 @@
-package com.minixiao.api.recruiter.controller.candidate;
-
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HandleHistoryCommand {
-
-}
